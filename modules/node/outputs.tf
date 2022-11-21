71c3c3100059883dfb586b9440209da474cd83c6
tompisula <tompisula@labrats.work>
2022-11-21 20:19:21 +0100

    mark node output sensitive

--- modules/node/outputs.tf
+++ modules/node/outputs.tf
@@ -1,8 +1,10 @@
 output "ipv4_address" {
-  value = hcloud_server.node.ipv4_address
+  value     = hcloud_server.node.ipv4_address
+  sensitive = true
 }
 output "name" {
-  value = local.hetzner.name
+  value     = local.hetzner.name
+  sensitive = true
 }
 output "nodetype" {
   value = local.hetzner.nodetype
