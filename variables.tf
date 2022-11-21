a11ef917c2c00c7e22048a09ce4b6e5e82e32855
tompisula <tompisula@labrats.work>
2022-11-21 20:05:10 +0100

    mark token sensitive

--- variables.tf
+++ variables.tf
@@ -1,4 +1,5 @@
 variable "hcloud_token" {
   description = "(Required) - The Hetzner Cloud API Token, can also be specified with the HCLOUD_TOKEN environment variable."
   type        = string
+  sensitive   = true
 }
