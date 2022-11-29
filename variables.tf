86c9573a35f2dd79b3ac497ff8c6d43ee465b26f
tompisula <tompisula@labrats.work>
2022-11-29 11:49:24 +0100

    workflow changes

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "hcloud_token" {
   type        = string
   sensitive   = true
 }
+
+variable "hcloud_resource_prefix" {
+  description = "(Required) - Prefix for resource names."
+  type        = string
+  sensitive   = true
+}
+
+variable "public_key" {
+  description = "(Required) - Public key for testing."
+  type        = string
+  sensitive   = true
+}
