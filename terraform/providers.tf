02e36d96ca6e7cf63326114e8b7138e825fa4454
tompisula <tompisula@labrats.work>
2022-12-07 17:39:28 +0100

    refactor

---
+++ terraform/providers.tf
@@ -0,0 +1,14 @@
+# providers.tf
+
+terraform {
+  backend "local" {}
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+    }
+  }
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "hcloud_token" {
+  description = "(Required) - The Hetzner Cloud API Token, can also be specified with the HCLOUD_TOKEN environment variable."
+  type        = string
+  sensitive   = true
+}
+
+variable "public_key" {
+  description = "(Required) - Public key for testing."
+  type        = string
+  sensitive   = true
+}
