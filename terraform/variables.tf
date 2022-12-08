9db309c9bc262fc36318a8631a1428f170a626c2
tompisula <tompisula@labrats.work>
2022-12-08 11:49:49 +0100

    trigger workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,5 @@
+# ./variables.tf
+
 variable "hcloud_token" {
   description = "(Required) - The Hetzner Cloud API Token, can also be specified with the HCLOUD_TOKEN environment variable."
   type        = string
