a780854327c836d2c7d7d8bf2baf72f3820784e1
tompisula <tompisula@labrats.work>
2022-12-24 15:51:15 +0100

    adding metrics

--- terraform/main.tf
+++ terraform/main.tf
@@ -54,6 +54,22 @@ module "worker_node_group" {
   }
 }
 
+module "metrics_node_group" {
+  source = "./modules/node_group"
+  depends_on = [
+    module.bastion_node_group
+  ]
+  nodes        = local.config_nodes_metrics
+  bastion_host = values(module.bastion_node_group.nodes)[0].ipv4_address
+  public_key   = var.public_key
+  networks_map = { for config_network in local.config_networks : config_network.id =>
+    {
+      name       = config_network.id,
+      hetzner_id = module.networks[config_network.id].hetzner_network.id
+    }
+  }
+}
+
 resource "local_file" "ansible_inventory_site" {
   content = templatefile("files/templates/site.tftpl", {
     bastion_nodes = [for node in module.bastion_node_group.nodes : {
@@ -65,6 +81,10 @@ resource "local_file" "ansible_inventory_site" {
       ansible_host = node.ipv4_address
     }]
     worker_nodes = [for node in module.worker_node_group.nodes : {
+      name         = node.name,
+      ansible_host = node.ipv4_address
+    }]
+    metrics_nodes = [for node in module.metrics_node_group.nodes : {
       name         = node.name,
       ansible_host = node.ipv4_address
     }]
@@ -83,6 +103,10 @@ resource "local_file" "ansible_inventory_cluster" {
       ansible_host = node.networks[module.networks["bnet"].hetzner_network.id].ip
     }]
     worker_nodes = [for node in module.worker_node_group.nodes : {
+      name         = node.name,
+      ansible_host = node.networks[module.networks["bnet"].hetzner_network.id].ip
+    }]
+    metrics_nodes = [for node in module.metrics_node_group.nodes : {
       name         = node.name,
       ansible_host = node.networks[module.networks["bnet"].hetzner_network.id].ip
     }]
